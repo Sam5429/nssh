3509974b28519eb47f576737b0fbec55bb798a99
sam <samuel.klein-bichain@etu.unistra.fr>
2025-04-20 21:06:06 +0200

    petit modif dans le code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 //mod aes;
 mod rsa;
 
+use std::process::exit;
+
 use rsa::{cypher_file, decypher_file, PrivateKey};
 
 fn main() {
@@ -8,6 +10,11 @@ fn main() {
     let public_key = private_key.pub_key.clone();
 
     let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        println!("syntax : cargo run <file> 'cypher|decypher'");
+        exit(-1);
+    }
     // affiche le contenur de repertoir courant
     let message_path = args[1].as_str();
     match args[2].as_str() {
